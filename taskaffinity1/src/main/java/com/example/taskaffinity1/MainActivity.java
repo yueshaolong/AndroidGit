ffd63eba0d0dc04db1656d549340185b935e30b2
yueshaolong <yueshaolong@vsxin.com>
2019-04-24 20:04:12 +0800

    android taskAffinity 和 allowTaskReparenting

--- taskaffinity1/src/main/java/com/example/taskaffinity1/MainActivity.java
+++ taskaffinity1/src/main/java/com/example/taskaffinity1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.taskaffinity1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        System.out.println("invoke onNewIntent");
+
+        setIntent(intent);//must store the new intent unless getIntent() will return the old one
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("invoke onRestart");
     }
 }
