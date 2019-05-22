e62915d2e003fee809cfc78f512c975076da7105
yueshaolong <yueshaolong@vsxin.com>
2019-05-22 18:13:34 +0800

    recycleview

--- app/src/main/java/com/ysl/recyclerview/RecyclerViewAdapter.java
+++ app/src/main/java/com/ysl/recyclerview/RecyclerViewAdapter.java
@@ -33,31 +33,30 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.recycler_view_item, parent, false);
+
+        //控件的点击事件建议写在这个方法里；只会设置一次；如果设置在onBindViewHolder()中，每次加载控件都会重新设置一个点击事件，性能损耗
+        click(view);
+
         return new MyHolder(view);
     }
+
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
         String name = nameList.get(position);
 
         holder.textView.setText(name);
-        holder.textView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "点击name", Toast.LENGTH_SHORT).show();
-            }
-        });
-        holder.itemView.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "点击条目", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        click(holder.itemView);//建议写在onCreateViewHolder()方法中。
     }
 
     @Override
     public int getItemCount() {
         return nameList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
     }
 
     public class MyHolder extends ViewHolder{
@@ -69,5 +68,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
+    }
+
+    private void click(View view) {
+        view.findViewById(R.id.tv).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "点击name", Toast.LENGTH_SHORT).show();
+            }
+        });
+        view.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "点击条目", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
