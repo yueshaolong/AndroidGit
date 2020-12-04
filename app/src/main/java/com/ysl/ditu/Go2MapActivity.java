1884bf865e0c539b6c8acbd3e3faae3423f28550
dragon <809629457@qq.com>
2020-12-04 13:37:16 +0800

    向各地图跳转功能

--- app/src/main/java/com/ysl/ditu/Go2MapActivity.java
+++ app/src/main/java/com/ysl/ditu/Go2MapActivity.java
@@ -57,11 +57,6 @@ public class Go2MapActivity extends AppCompatActivity {
             arg.put(DESTINATION,"天安门");
             for (String packageName : packages) {
                 switch (packageName) {
-                    case BAIDUMAP_PACKAGENAME:
-                        arg.put(GCJO2_LNG,"116.39752865");
-                        arg.put(GCJO2_LAT,"39.90873221");
-                        invokeBaiDuMap(this, arg);
-                        return;
                     case AUTONAVI_PACKAGENAME:
                         arg.put(GCJO2_LNG,"116.39752865");
                         arg.put(GCJO2_LAT,"39.90873221");
@@ -71,7 +66,12 @@ public class Go2MapActivity extends AppCompatActivity {
                         arg.put(GCJO2_LNG,"116.39752865");
                         arg.put(GCJO2_LAT,"39.90873221");
                         invokeQQMap(this, arg);
-                        break;
+                        return;
+                    case BAIDUMAP_PACKAGENAME:
+                        arg.put(GCJO2_LNG,"116.39752865");
+                        arg.put(GCJO2_LAT,"39.90873221");
+                        invokeBaiDuMap(this, arg);
+                        return;
                 }
             }
         }
@@ -146,7 +146,7 @@ public class Go2MapActivity extends AppCompatActivity {
         }
     }
 
-    private static final String[] MAP_PACKAGES = {BAIDUMAP_PACKAGENAME, AUTONAVI_PACKAGENAME, QQMAP_PACKAGENAME};
+    private static final String[] MAP_PACKAGES = {AUTONAVI_PACKAGENAME, BAIDUMAP_PACKAGENAME, QQMAP_PACKAGENAME};
     /**
      * 检查手机上是否安装了指定的软件
      *
@@ -180,4 +180,48 @@ public class Go2MapActivity extends AppCompatActivity {
         return newPackageNames;
     }
 
+    /**
+     * 16进制直接转换成为字符串(无需Unicode解码)
+     * @param hexStr
+     * @return
+     */
+    public static String hexStr2Str(String hexStr) {
+        String str = "0123456789abcdef";
+        char[] hexs = hexStr.toCharArray();
+        byte[] bytes = new byte[hexStr.length() / 2];
+        int n;
+        for (int i = 0; i < bytes.length; i++) {
+            n = str.indexOf(hexs[2 * i]) * 16;
+            n += str.indexOf(hexs[2 * i + 1]);
+            bytes[i] = (byte) (n & 0xff);
+        }
+        return new String(bytes);
+    }
+
+    /**
+     * 16进制转换成为string类型字符串
+     * @param s
+     * @return
+     */
+    public static String hexStringToString(String s) {
+        if (s == null || s.equals("")) {
+            return null;
+        }
+        s = s.replace(" ", "");
+        byte[] baKeyword = new byte[s.length() / 2];
+        for (int i = 0; i < baKeyword.length; i++) {
+            try {
+                baKeyword[i] = (byte) (0xff & Integer.parseInt(s.substring(i * 2, i * 2 + 2), 16));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            s = new String(baKeyword, "UTF-8");
+//            new String();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return s;
+    }
 }
