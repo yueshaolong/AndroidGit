e01bc3e72d5d617ada4d8167bb669c26ce341675
dragon <809629457@qq.com>
2020-12-18 16:19:28 +0800

    截屏

---
+++ app/src/main/java/com/ysl/jieping/JiePingActivity.java
@@ -0,0 +1,17 @@
+package com.ysl.jieping;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ysl.helloworld.R;
+
+public class JiePingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_jieping);
+
+    }
+}
